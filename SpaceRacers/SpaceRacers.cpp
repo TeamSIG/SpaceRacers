cd08191de805cd0394b3bc2f818bc50b09262f36
Costa Florent <costa_florent@hotmail.fr>
2015-04-15 11:13:19 +0200

    initial commit

---
+++ SpaceRacers/SpaceRacers.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SpaceRacers.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceRacers, "SpaceRacers" );
---
+++ SpaceRacers/SpaceRacersGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SpaceRacersGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACERACERS_API ASpaceRacersGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
